47cda7686d7d07fc877f974c3bb410d18c37f277
thengoc12 <22726651.ngoc@student.iuh.edu.vn>
2024-08-15 20:10:01 +0700

    Update Welcome.java

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,6 +1,6 @@
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Hello Tang The Ngoc!");
-        System.out.println("Welcome to the DHKHMT18A!");
+        System.out.println("Welcome to the DHHTTT19A!");
     }
 }
